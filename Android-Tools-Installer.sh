78ac0d4be66d63c77fe5517262636903b87e9bf8
Eric Luce <eluce11232011@gmail.com>
2026-03-15 19:30:45 -0400

    Delete Android-Tools-Installer.sh

--- Android-Tools-Installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "This Installer Prepares Your Arch System For Nearly EVERY Android Device!"
-sleep 5
-sudo pacman -Syu --noconfirm
-sudo pacman -S usb_modeswitch android-tools android-udev mtpfs libmtp gvfs-mtp heimdall --noconfirm
-clear
-echo "Done!"
