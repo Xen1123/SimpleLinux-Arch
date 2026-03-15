d38d341552af2a0a5de2c14e6b6630635bd91ee5
Eric Luce <eluce11232011@gmail.com>
2026-03-15 19:33:52 -0400

    Update App-Installer-Basic-KDE.sh

--- Arch/App-Installer-Basic-KDE.sh
+++ Arch/App-Installer-Basic-KDE.sh
@@ -2,7 +2,7 @@
 cd /home/$USER
 echo "This script installs a very basic, yet necessary group of apps for your Arch Linux system! It is not a script for installing Arch Linux itself!"
 sleep 3
-sudo pacman -S firefox sddm fastfetch networkmanager openssh nano vim plasma android-tools gvfs gvfs-mtp yt-dlp konsole dolphin base-devel git wget curl usbutils --needed --noconfirm
+sudo pacman -S firefox efibootmgr fwupd sddm fastfetch networkmanager openssh nano vim plasma android-tools gvfs gvfs-mtp yt-dlp konsole dolphin base-devel git wget curl usbutils --needed --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
